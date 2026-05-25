8fa1d7b78ae21a88451f3792abe8837672309e77
Conor <conormcgrath@gmail.com>
2026-05-25 20:47:19 +0100

    Update application routing

--- public/index.php
+++ public/index.php
@@ -1,3 +1,69 @@
 <?php
+session_start();
 
-echo "Swiftqueue Application";
+require_once __DIR__ . '/../src/Controllers/AuthController.php';
+require_once __DIR__ . '/../src/Controllers/TaskController.php';
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if ($uri === '/' || $uri === '/login') 
+{
+    $controller = new AuthController();
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') 
+    {
+
+        $controller->login();
+
+    } else 
+    {
+        $controller->showLogin();
+    }
+
+    exit;
+}
+
+if ($uri === '/tasks') 
+{
+    $controller = new TaskController();
+
+    $controller->index();
+
+    exit;
+}
+
+if ($uri === '/tasks/create') 
+{
+    $controller = new TaskController();
+
+    $controller->create();
+    
+    exit;
+}
+
+if ($uri === '/tasks/update') {
+
+    $controller = new TaskController();
+    
+    $controller->update();
+    
+    exit;
+}
+
+if ($uri === '/tasks/delete' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $controller = new TaskController();
+    
+    $controller->delete();
+    
+    exit;
+}
+
+if ($uri === '/logout') {
+
+    $controller = new AuthController();
+
+    $controller->logout();
+
+    exit;
+}
